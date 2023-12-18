438e8155c6b3cb246ead841282ade6d2c3315122
Alireza Ataee <alireza.ataee01@gmail.com>
2023-12-18 08:04:31 -0500

    complete breadcrumb

--- fragments/Breadcrumb.tsx
+++ fragments/Breadcrumb.tsx
@@ -1,15 +1,15 @@
 import { Breadcrumb } from '@/types/breadcrumb'
 import Link from 'next/link'
 import React from 'react'
-import { FiArrowRight } from 'react-icons/fi'
+import { FiArrowLeft } from 'react-icons/fi'
 
 const Breadcrumb: React.FC<Breadcrumb> = ({ items }) => {
 	return (
-    <nav>
+    <nav className='flex flex-row items-center my-3'>
       {items.map((item, key) => (
-        <div key={key}>
+        <div key={key} className='flex flex-row  items-center'>
           <Link href={item.link}>{item.title}</Link>
-          <span><FiArrowRight /></span>
+          <span className={`${key+1 == items.length ? 'hidden' : ''} px-3`}><FiArrowLeft className='text-sm' /></span>
         </div>
       ))}
     </nav>
