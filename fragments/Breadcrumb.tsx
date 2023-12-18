b4ef84b4506a5f0ebf7ee2c6bd2103a6263217c6
Alireza Ataee <alireza.ataee01@gmail.com>
2023-12-18 08:28:43 -0500

    make the last item of breadcumb clear

--- fragments/Breadcrumb.tsx
+++ fragments/Breadcrumb.tsx
@@ -4,14 +4,18 @@ import React from 'react'
 import { FiArrowLeft } from 'react-icons/fi'
 
 const Breadcrumb: React.FC<Breadcrumb> = ({ items }) => {
-	return (
+  return (
     <nav className='flex flex-row items-center my-3'>
       {items.map((item, key) => (
-        <div key={key} className='flex flex-row  items-center'>
+        // on small screen width, it will replace the last item in breadcumb with ... since it already is the current page
+        // that's the least I can do due to the lack of instructions
+        <div key={key} className={`flex flex-row  items-center ${key+1 == items.length ? 'sm:!flex hidden' : ''}`}>
           <Link href={item.link}>{item.title}</Link>
           <span className={`${key+1 == items.length ? 'hidden' : ''} px-3`}><FiArrowLeft className='text-sm' /></span>
         </div>
       ))}
+      {/* here is the ... for small screens */}
+      <div className='sm:!hidden flex'><Link href={items[items.length-1].link}>...</Link></div>
     </nav>
 	)
 }
