1204aae47acbd9420294ecb1bbf19c99c37329f0
Alireza Ataee <alireza.ataee01@gmail.com>
2023-12-18 08:05:09 -0500

    setup Breadcrumb

--- pages/[pricelist_slug].tsx
+++ pages/[pricelist_slug].tsx
@@ -1,30 +1,28 @@
 "use client"
 
 import Breadcrumb from "@/fragments/Breadcrumb"
-import { Breadcrumb as Breadcrumb_d } from "@/types/breadcrumb"
 
 interface Props {
-  currentPage: CurrentPage,
-  // breadcrumb: Breadcrumb_d,
+  currentPage: PageBasicInfo,
+  parentPage: PageBasicInfo,
 }
 
-interface CurrentPage {
+interface PageBasicInfo {
   title: string,
   slug: string,
   link: string,
 }
 
-const Pricelist: React.FC<Props> = ({ currentPage }) => {
+const Pricelist: React.FC<Props> = ({ currentPage, parentPage }) => {
   return (
-    <main className='flex flex-col bg-slate-100 dark:bg-slate-700'>
+    <main className='flex flex-col bg-slate-100'>
       <Breadcrumb items={[
         {
           title: 'خانه',
           link: '#',
-        },{
-          title: currentPage.title,
-          link: currentPage.link,
-        }
+        },
+        parentPage,
+        currentPage
       ]} />
     </main>
   )
@@ -46,9 +44,13 @@ export async function getStaticProps({params}: any) {
   const props: Props = {
     currentPage: {
       title: 'لیست قیمت میلگرد آجدار',
-      slug: 'ajdar-milgerd',
-      link: '/products/ajdar-milgerd'
+      slug: 'ajdar',
+      link: '/milgerd/ajdar'
     },
+    parentPage: {
+      title: 'لیست قیمت میلگرد',
+      slug: 'milgerd',
+      link: '/milgerd'    }
   }
 
   return {
