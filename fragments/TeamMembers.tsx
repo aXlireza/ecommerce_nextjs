29eef8fe0f076f30868589cfa014bf02f594fa5d
Alireza Ataee <alireza.ataee01@gmail.com>
2023-12-19 10:16:30 -0500

    minor code cleanup

--- fragments/TeamMembers.tsx
+++ fragments/TeamMembers.tsx
@@ -15,7 +15,7 @@ const TeamMembers: React.FC<Props> = ({ teams }) => {
 		<section className='px-10 lg:px-24 py-10 bg-white'>
       <div className='rounded-lg bg-white border-2 border-slate-samen'>
         <div className='flex flex-row gap-3 justify-center'>
-          {teams.map(item => item.title).map((title, key) => <Heading key={key} underline={key == activeTab ? 'full' : null} text={title} color='border-yellow-600' customClasses='cursor-pointer' onClick={() => setActiveTab(key)} textClasses='' />)}
+          {teams.map(item => item.title).map((title, key) => <Heading key={key} underline={key == activeTab ? 'full' : undefined} text={title} color='border-yellow-600' customClasses='cursor-pointer' onClick={() => setActiveTab(key)} textClasses='' />)}
         </div>
         <hr className='border-2 border-slate-samen' />
         <div className='flex flex-col gap-3 justify-center py-5'>
