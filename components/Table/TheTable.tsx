b9348c0fba31df7294984304a899aca6dc70385f
Alireza Ataee <alireza.ataee01@gmail.com>
2023-12-20 04:09:55 -0500

    fine tune color

--- components/Table/TheTable.tsx
+++ components/Table/TheTable.tsx
@@ -9,8 +9,9 @@ const colStyle = "font-medium text-center"
 const TheTable: React.FC<Props> = ({ products }) => {
 	return (<table className="table-auto rounded-lg overflow-hidden">
     {/* table head */}
-    <thead>
-      <tr className="bg-samen-orange h-10">
+    <thead className="relative">
+      <div className="bg-samen-orange opacity-50 absolute w-full h-full"></div>
+      <tr className="h-12 relative">
         <th className={colStyle}>کالا</th>
         <th className={colStyle}>کارخانه</th>
         <th className={colStyle}>سایز</th>
@@ -20,7 +21,7 @@ const TheTable: React.FC<Props> = ({ products }) => {
     </thead>
     {/* table body */}
     <tbody>
-      {products.map((product, key) => <tr className={`odd:samen-lightbg even:bg-white py-2 h-10`} key={key}>
+      {products.map((product, key) => <tr className={`odd:samen-lightbg even:bg-white py-2 h-12`} key={key}>
         <td className={colStyle}>{product.title}</td>
         <td className={colStyle}>{product.factoryName}</td>
         <td className={colStyle}>{product.size}</td>
