e34eab69c85861bea0ca4f2786895b9a48956bbd
Alireza Ataee <alireza.ataee01@gmail.com>
2023-12-20 09:01:03 -0500

    minor update

--- fragments/Filter/Usage.tsx
+++ fragments/Filter/Usage.tsx
@@ -1,3 +1,12 @@
-{/* <section className={`divide-y samen-border bg-white`}>
-            <Heading text={`جستجوی سریع ${title}`} underline={'partial'} />
-          </section> */}
+import Heading from "@/components/Heading"
+
+interface Props {
+
+}
+const Usage: React.FC<Props> = ({  }) => {
+  return (<section className={`divide-y samen-border bg-white`}>
+    <Heading text={`جستجوی سریع`} underline={'partial'} />
+  </section>)
+}
+
+export default Usage
