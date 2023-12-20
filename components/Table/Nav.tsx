81e2593097568886eb65175fc073ba883afc5198
Alireza Ataee <alireza.ataee01@gmail.com>
2023-12-20 04:27:45 -0500

    Adding the outdated state

--- components/Table/Nav.tsx
+++ components/Table/Nav.tsx
@@ -2,24 +2,24 @@ import { Nav } from '@/types/Table';
 import React from 'react'
 import { FiCheck, FiClock } from 'react-icons/fi';
 
-const TableNav: React.FC<Nav> = ({ rateActive=false, title, date, rateFunc }) => {
+const TableNav: React.FC<Nav> = ({ rateActive=false, title, date, rateFunc, upToDate=true }) => {
 	return (
     <section className="relative w-full flex items-center bg-white justify-between overflow-hidden samen-border">
 			{/* right part */}
 			<div className='flex flex-row items-center'>
-				<div className="z-10 bg-samen-orange w-fit h-[50px] flex items-center justify-center pr-5">
+				<div className={`z-10 bg-${upToDate ? 'samen-orange' : 'samen-gray'} w-fit h-[50px] flex items-center justify-center pr-5`}>
 					<p className="text-center font-semibold samen-text text-sm sm:text-base">{title}</p>
 				</div>
 				{/* triangle */}
-				<span className="w-0 h-0 
+				<span className={`w-0 h-0 
 					border-t-[50px] border-t-transparent
-					border-r-[40px] border-r-samen-orange
-					border-b-[0px] border-b-transparent">
+					border-r-[40px] border-r-${upToDate ? 'samen-orange' : 'samen-gray'}
+					border-b-[0px] border-b-transparent`}>
 				</span>
 
 				{/* date */}
 				<article className='flex flex-row items-center gap-1 -mr-2'>
-					<FiClock className="stroke-samen-orange" />
+					{upToDate ? <FiClock className="stroke-samen-orange" /> : null}
 					<span className='samen-text text-sm sm:text-base'>{date}</span>
 				</article>
 			</div>
