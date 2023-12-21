de23ebbfb6f9f1041b2fb30cc5d885f879b6a278
Alireza Ataee <alireza.ataee01@gmail.com>
2023-12-21 04:32:18 -0500

    Fixing the style of the label beside select

--- components/Form/CustomSelect.tsx
+++ components/Form/CustomSelect.tsx
@@ -13,9 +13,9 @@ type Select = {
 }
 
 export default function CustomSelect({ options, fullyRounded, id, label }: Props) {
-	return (<div className={`w-full samen-border ${fullyRounded ? '!rounded-full' : ''}`}>
+	return (<div className={`w-full flex flex-row items-center gap-8`}>
     {id && label ? <label htmlFor={id}>{label}</label> : null}
-    <select className={`w-full samen-text bg-white p-4 ${fullyRounded ? 'rounded-full' : 'rounded-lg'} outline-none ps-4 shadow-samen`}>
+    <select id={id || ''} className={`w-full samen-text bg-white p-4 samen-border ${fullyRounded ? '!rounded-full' : ''} outline-none ps-4 shadow-samen`}>
       {options.map(({text, value}, key) => <option key={key} value={value}>{text}</option>)}
     </select>
   </div>)
