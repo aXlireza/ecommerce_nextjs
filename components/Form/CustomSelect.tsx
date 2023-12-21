eb65e07e7f26e36f4c22e476f397cd73186db716
Alireza Ataee <alireza.ataee01@gmail.com>
2023-12-21 04:23:22 -0500

    Adding the Label option

--- components/Form/CustomSelect.tsx
+++ components/Form/CustomSelect.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 type Props = {
   options: Select[],
-  fullyRounded?: boolean
+  fullyRounded?: boolean,
+  id?: string,
+  label?: string,
 }
 
 type Select = {
@@ -10,8 +12,9 @@ type Select = {
   value: string
 }
 
-export default function CustomSelect({ options, fullyRounded }: Props) {
+export default function CustomSelect({ options, fullyRounded, id, label }: Props) {
 	return (<div className={`w-full samen-border ${fullyRounded ? '!rounded-full' : ''}`}>
+    {id && label ? <label htmlFor={id}>{label}</label> : null}
     <select className={`w-full samen-text bg-white p-4 ${fullyRounded ? 'rounded-full' : 'rounded-lg'} outline-none ps-4 shadow-samen`}>
       {options.map(({text, value}, key) => <option key={key} value={value}>{text}</option>)}
     </select>
